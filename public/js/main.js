71778ada582591a8fca5795e41451d9f6399447f
Steven Barragan <me@steven.mx>
2012-09-14 03:14:10 -0500

    Adding principal structure

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
+    
+    var width
+    
+    var container = $("#container")
 
-	windows = $(window)
+    height =  480
 
-	width = windows.width()
-	width = width - 40
-	height = windows.height() - 100
-
-	$("#container").css({
-		'height' : height,
-		'width' : width
-	})
+    plot = $("#plot").width(function(){
+        width = container.width() - 200
+        width = width - ( width % 50 )
+        return width 
+    })
+    
+    $("#controls").width(function(){
+        return container.width() - width
+    })
+    
+    var paper = new Raphael(document.getElementById('plot'), plot.width() , plot.height() );
+    var map = paper.set();
+    
+    var patron, patron2
+    for(var i = 50; i < width ; i+=50) {
+        patron = "M " + i + " 0 l 0 " + height
+        patron2 = "M 0 " + i + " l " + width + " 0"
+        map.push( paper.path(patron) )
+        map.push( paper.path(patron2) )
+    }
+    
+    map.attr({
+        opacity: 0.5
+    })
 })
