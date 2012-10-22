c008137f0e347ace5ed4c808cbe5bcd9a0aa6a39
Steven Barragan <me@steven.mx>
2012-10-21 22:25:19 -0500

    Mesages updated

--- public/js/steptwo/main.js
+++ public/js/steptwo/main.js
@@ -19,25 +19,25 @@ $(document).ready(function(){
     
     plot.init()
 
-    $("#set_grid").click(function(){ plot.set_grid() })
+    $("#set_grid").click(function(){ plot.set_grid()})
     
-    $("#house").click(function(){ plot.set_house() })
+    $("#house").click(function(){ plot.set_house()})
 
-    $("#set_kibbus").click(function(){ plot.set_kibbus() })
+    $("#set_kibbus").click(function(){ plot.set_kibbus()})
 
-    $("#go_home").click(function(){ kibbus.search_home() })
+    $("#go_home").click(function(){ kibbus.search_home()})
 
     $("#show_flags").click(function(){plot.show_flags()})
     
     $("#instruction").popover({
-        title : "Try to lose Kibbus",
-        content : "<b>First:</b> Click on \"Set house button\", then click on the forest you want the house.<br /><b>Second:</b> Try to lose Kibbus: Keys to move him.<br /><pre>Q W E\nA   D\nZ X C</pre>\n<b>Third</b>: Click on \"Come back\" and watch if Kibbus is lost.",
+        title : "What to do?",
+        content : "<b>First:</b> Click on \"Set house button\", then click on the forest where you want the house.<br /><b>Second:</b> Click where you want to Kibbus start to look for the house.<br /><b>Third:</b> Click on \"Go home\" and watch how kibbus do his maximum effort to get home.",
         placement : "bottom"
     })
     
     $("#why").popover({
         title : "My AI school project",
-        content : "This is the first step to make Kibbus intelligent, today he only have \"memory\", a so basic intelligent way, but it let him come back home.<br />Step by step Kibbus is going to be getting smarter and smarter, he will have another ways of intelligent.<br />Please let me know if you find some bugs :D",
+        content : "This is the second step to make Kibbus intelligent, now Kibbus knows his house's position, he has to look for a way through the forest, Kibbus always draws a straight line between him and the house to follow and when come across an obstacle, he moves to another place and he tries to follow a new straight line to get home.<br />He has a system of flags to know the places where he already has been, if the flag is red is considered as a bad place to go and Kibbus are not gonna be there again.<br />Please if you find some bug let me know :D",
         placement : "bottom"
     })
 })
